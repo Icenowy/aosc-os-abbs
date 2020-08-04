753b0ab1aaa499e68840a6e91ab8043948ce9459
Icenowy Zheng <icenowy@aosc.io>
2020-08-05 07:20:22 +0800

    libjpeg-turbo+armhf: add
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

---
+++ extra-optenvarmhf/libjpeg-turbo+armhf/autobuild/armhf.cmake
@@ -0,0 +1,6 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(tools /opt/abcross/optenvarmhf)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-aosc-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-aosc-linux-gnueabihf-g++)
